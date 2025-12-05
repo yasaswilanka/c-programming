186afb813fd3ade0ae44296e37d1448c172b97e0
Yasaswi <yasaswilanka@gmail.com>
2025-12-05 15:53:47 +0530

    Add files via upload

---
+++ +&- neutral.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter the number: ");
+	scanf("%d",&n);
+	if(n>0)
+	{
+	printf("n is  positive number");}
+	else
+	if(n<0)
+	printf("\n n is negetive  number");
+	else
+	printf("number is neutral");
+	
+	
+}

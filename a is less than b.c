4bed186c78c62003d285cdbbbf0b070e3dba7949
Yasaswi <yasaswilanka@gmail.com>
2025-12-05 15:54:14 +0530

    Add files via upload

---
+++ a is less than b.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+	int a;
+	int b;
+	printf("Enter the value of a:");
+	scanf("%d",&a);
+    printf("Enter the value of b:");
+	scanf("%d",&b);
+	//peforming operations on a,b
+	printf("a is less than b",a<b);
+	
+
+	
+}

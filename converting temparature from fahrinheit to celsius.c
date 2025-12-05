dfb5e4d3005a7001dc9b477bb2031d9cd97ee5fb
Yasaswi <yasaswilanka@gmail.com>
2025-12-05 14:35:07 +0530

    Add files via upload

---
+++ converting temparature from fahrinheit to celsius.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	int fh,c;
+	printf("Enter the temparature in fahrenheit:");
+	scanf("%d",&fh);
+	c=(fh-32)*5/9;
+	printf("the temparature in celsus is %d",c);
+	printf("/n);
+}
+
+

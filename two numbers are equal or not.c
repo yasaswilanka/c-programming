6adb68ab35c867ee78593fcf664057a909af16d0
Yasaswi <yasaswilanka@gmail.com>
2025-12-05 15:53:20 +0530

    Add files via upload

---
+++ two numbers are equal or not.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter the values of a: ");
+	scanf("%d",&a);
+	printf("enter the values of b: ");
+	scanf("%d",&b);
+	if
+	(a==b)
+	printf("two numbers are equal");
+	else
+	printf("\n not equal");
+	
+	
+}

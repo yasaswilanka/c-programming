f944e4d35aa7315437331ffcaddea4d519450fad
Yasaswi <yasaswilanka@gmail.com>
2025-12-05 15:54:48 +0530

    Add files via upload

---
+++ addition of two numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+main()
+{
+	int a,b,res;
+	printf("Enter the value ofa:");
+	scanf("%d",&a);
+	printf("Enter th value of b:");
+	scanf("%d",&b);
+	res=a+b;
+	printf("the result is %d",res);
+}

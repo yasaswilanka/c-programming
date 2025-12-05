d9a8522e33f1387bda96e93d5720d37cf743991c
Yasaswi <yasaswilanka@gmail.com>
2025-12-05 15:50:27 +0530

    Add files via upload

---
+++ pass&fail.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int mat=0;
+	printf("Enter maths marks :   ");
+	scanf("%d",&mat);
+	if(mat>=40)
+	printf("you are passed \n congratulations....");
+	else
+	printf("you are failed \n bad luck......");
+	printf("visit school once \n");
+	printf("\n");
+}

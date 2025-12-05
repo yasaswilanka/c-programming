bac822401bf3737394ba62de7f965fdf55e002d2
Yasaswi <yasaswilanka@gmail.com>
2025-12-05 14:35:44 +0530

    Add files via upload

---
+++ Eligibile for vote or not.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int age =0;
+	printf("Enter your age :..");
+	scanf("%d",&age);
+	if(age>18)
+	printf("Eligible for vote ");
+	else
+	printf("Not eligbile vote");
+	printf("\n");
+	
+}

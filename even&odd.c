6aa1a9ca6662057c3aa9cd2754961e3f11e9fca9
Yasaswi <yasaswilanka@gmail.com>
2025-12-05 14:36:29 +0530

    Add files via upload

---
+++ even&odd.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int N=0;
+	printf("Enter the value of N:");
+	scanf("%d",&N);
+	if(N%2==0)
+	printf("N is even number");
+	else
+	printf("N is odd number");
+	printf("\n");
+}

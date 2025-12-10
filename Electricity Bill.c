331fcf595d0e6e9e93c8df35b2d7a9b0207df969
Yasaswi <yasaswilanka@gmail.com>
2025-12-10 20:09:02 +0530

    Add files via upload

---
+++ Electricity Bill.c
@@ -0,0 +1,15 @@
+//Electicity Bill
+#include<stdio.h>
+int main()
+{
+int a;
+printf("enter no.of units");
+sacanf("%d",&a);
+ int price=100;
+int amount;
+char name[30]="kamal kiran";
+printf("costumer name %s \n",name);
+amount=a*price;
+printf("amount of bill %d",amount);	
+return 0;
+}

12240d8e6dcf1a61aa715d3169975d75d9a4fd47
Yasaswi <yasaswilanka@gmail.com>
2025-12-10 19:58:29 +0530

    Add files via upload

---
+++ multiplication of two numberrs.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{int x,y,res;
+printf("Enter the value of x:");
+scanf("%d",&x);
+printf("Enter the value of y:");
+scanf("%d",&y);
+res=x*y;
+printf(" \n the result is %d :",res);
+	
+}
+

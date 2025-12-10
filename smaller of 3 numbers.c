4a567ebd2a0ee63b337820a139b5bd09563d270d
Yasaswi <yasaswilanka@gmail.com>
2025-12-10 19:59:17 +0530

    Add files via upload

---
+++ smaller of 3 numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter the values of a: ");
+	scanf("%d",&a);
+	printf("enter the values of b: ");
+	scanf("%d",&b);
+	printf("enter the values of c: ");
+	scanf("%d",&c);
+	if
+	(a<b){
+	printf("a is smaller number");
+	}
+	else
+	if(b<c)
+	printf("b is smaller number");
+	else
+	printf("c is smaller number");
+	
+	
+}

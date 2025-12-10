08ebe4b443c73d3bd83f73998c1e92f9ed908b52
Yasaswi <yasaswilanka@gmail.com>
2025-12-10 20:12:46 +0530

    Add files via upload

---
+++ biggest of 2 numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a,b=0;
+	printf("Enter the value of a:");
+	scanf("%d",&a);
+	printf("Enter the value of b:");
+	scanf("%d",&b);
+	if(a>b)
+	printf("a is bigger number \n");
+	else
+	printf("b is bigger number \n");
+	
+}
